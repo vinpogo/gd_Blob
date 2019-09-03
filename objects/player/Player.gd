01965f31044189d891300770b33a1d69630961eb
vinpogo <vin.grossmayer@hotmail.com>
2019-09-03 22:41:04 +0200

    added bounce, multijump behaviour and level

--- objects/player/Player.gd
+++ objects/player/Player.gd
@@ -6,6 +6,7 @@ export var JUMP_FACTOR = 0.5
 export var MAX_JUMPS = 3
 
 var onFloor = true
+var toBounce = false
 
 var jump_direction = Vector2()
 var jump_count = 0
@@ -23,8 +24,24 @@ func is_touching():
 func _ready():
 	newCollide = move_and_collide(Vector2(0.1,0))
 	oldCollide = move_and_collide(Vector2(-0.1,0))
-
-func jump(dir):
+	
+func thirdJump(dir):
+	onFloor = false
+	jump_count += 1
+	if dir.normalized().dot(gravity_dir.normalized()) > 0:
+		velocity += dir.normalized() * JUMP_FORCE
+	else:
+		velocity = dir.normalized() * JUMP_FORCE * 1.2
+	
+func secondJump(dir):
+	onFloor = false
+	jump_count += 1
+	if dir.normalized().dot(gravity_dir.normalized()) > 0:
+		velocity += dir.normalized() * JUMP_FORCE
+	else:
+		velocity = dir.normalized() * JUMP_FORCE * 1.1
+	
+func firstJump(dir):
 	onFloor = false
 	jump_count += 1
 	velocity = dir.normalized() * JUMP_FORCE
@@ -34,13 +51,20 @@ func jumpHandler():
 	if is_touching() && jump_count == 0:
 		$AnimatedSprite.play("air-jump")
 		jump_direction = get_global_mouse_position() - position
-		jump(jump_direction)
+		firstJump(jump_direction)
 		return
-	if!is_touching() && jump_count <= MAX_JUMPS:
-		$AnimatedSprite.play("ground-jump")
-		jump_direction = get_global_mouse_position() - position
-		jump(jump_direction)
-		return
+	if!is_touching():
+		if jump_count == 1:
+			$AnimatedSprite.play("ground-jump")
+			jump_direction = get_global_mouse_position() - position
+			secondJump(jump_direction)
+			return
+			
+		elif jump_count == 2:
+			$AnimatedSprite.play("ground-jump")
+			jump_direction = get_global_mouse_position() - position
+			thirdJump(jump_direction)
+			return
 
 func gravityFactor():
 	if Input.is_action_pressed("jump") && !is_falling_down():
@@ -52,16 +76,22 @@ func is_falling_down():
 	return velocity.normalized().dot(gravity_dir.normalized()) > 0
 
 func _process(delta):
-	#inputHandler()
 	velocity += (gravity_dir * GRAVITY * gravityFactor()) * delta
 	if !onFloor:
 		rotation = velocity.angle() + PI/2
 	newCollide = move_and_collide(velocity)
 	if (newCollide != oldCollide):
 		if newCollide:
-			land()
+			if toBounce:
+				bounce(newCollide)
+			else:
+				land()
 		oldCollide = newCollide
 		
+func bounce(col):
+	velocity = velocity.bounce(col.normal)*0.5
+	toBounce = false
+	
 func land():
 	$AnimatedSprite.play("idle")
 	onFloor = true
@@ -72,10 +102,17 @@ func land():
 	gravity_dir.y = round(-newCollide.normal.y)
 	rotation = gravity_dir.angle() - PI/2
 
-
 func _input(event):
 	if event.is_action_pressed("jump"):
 		jumpHandler()
 
 func ru_position():
 	return self.position
+
+
+func _on_BounceArea_bounce():
+	toBounce = true
+
+func _on_BounceArea_stopBounce():
+	return
+	toBounce = false
