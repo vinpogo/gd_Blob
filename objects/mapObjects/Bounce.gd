2ca4c7b757af866cb23937f401d35d40d9185fa9
vinpogo <vin.grossmayer@hotmail.com>
2019-09-23 21:21:32 +0200

    bounce

--- objects/mapObjects/Bounce.gd
+++ objects/mapObjects/Bounce.gd
@@ -1,4 +1,31 @@
 extends StaticBody2D
 
+onready var sprite = get_node("Sprite")
+export(String, "bouncy", "sticky") var type = "sticky"
+var currentType
+var bouncy = preload("res://gfx/standard_circle_bounce.png")
+var sticky = preload("res://gfx/standard_circle.png")
+
+func ru_setTexture(type):
+	match(type):
+		"sticky":
+			sprite.set_texture(sticky)
+			currentType = "sticky"
+		"bouncy":
+			sprite.set_texture(bouncy)
+			currentType = "bouncy"
+			
+			
+
+
+func _ready():
+	ru_setTexture(type)
+
+func _physics_process(delta):
+	print(type)
+	if type != currentType:
+		print("switch sprite")
+		ru_setTexture(type)
+		
 func get_type():
-	return "bounce"
+	return type
